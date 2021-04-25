138238a7a3510ae0a1b546711bd45e4a50980342
carloslavie <carloslavie@gmail.com>
2021-04-25 01:02:10 -0300

    Header, nav, Hero

---
+++ src/pages/nosotros.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Layout from '../components/Layout';
+
+
+const Nosotros = () => (
+  <Layout>
+    <h1>Nosotros</h1>
+  </Layout>
+)
+
+export default Nosotros
